9006b73e2cde28ea15742dbfe9fb55bf181e5e0d
nimrodnm <nimrod.bargiora@mail.huji.ac.il>
2022-05-22 22:39:05 +0300

    Saved the plots and wrote bullshit answers to the questions in the practical part

--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -25,19 +25,19 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
     # Question 1 - Generate dataset for model f(x)=(x+3)(x+2)(x+1)(x-1)(x-2) + eps for eps Gaussian noise
     # and split into training- and testing portions
     poly = lambda x: (x + 3) * (x + 2) * (x + 1) * (x - 1) * (x - 2)
-    x = np.random.uniform(-1.2, 2, n_samples)
-    x_sorted = np.linspace(-1.2, 2, n_samples)
+    x = np.linspace(-1.2, 2, n_samples)
     epsilon = np.random.normal(0, noise, n_samples)
     y = poly(x) + epsilon
     train_x, train_y, test_x, test_y = split_train_test(pd.DataFrame(x), pd.Series(y), train_proportion=(2 / 3))
     train_x, test_x = train_x.squeeze(), test_x.squeeze()
-    fig_1 = go.Figure([go.Scatter(x=x_sorted, y=poly(x_sorted), mode="lines",
+    fig_1 = go.Figure([go.Scatter(x=x, y=poly(x), mode="lines",
                                   name="True Noiseless Model", line_color="black"),
                        go.Scatter(x=train_x, y=train_y, mode="markers", name="Train Data", marker_color="blue"),
                        go.Scatter(x=test_x, y=test_y, mode="markers", name="Test Data", marker_color="red")])
     fig_1.update_layout(title=f"Scatter Plot of Noiseless Model and of Test and Train Data<br>"
                               f"Noise = {noise}, Number of Samples = {n_samples}",
                         xaxis_title="x", yaxis_title="y")
+
     fig_1.show()
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
@@ -52,7 +52,7 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
                        go.Scatter(x=degrees, y=validation_errors, mode="markers + lines", name="Validation Errors")])
     fig_2.update_layout(title=f"Train and Validation Errors of 5-Fold Cross-Validation on Degrees k=0,1,...,10<br>"
                               f"Noise = {noise}, Number of Samples = {n_samples}",
-                        xaxis_title="k", yaxis_title="Error Value")
+                        xaxis_title="k", yaxis_title="Error Value", xaxis_type='category')
     fig_2.show()
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
@@ -84,7 +84,7 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
     train_y, test_y = train_y.to_numpy(), test_y.to_numpy()
 
     # Question 7 - Perform CV for different values of the regularization parameter for Ridge and Lasso regressions
-    lambdas_range_start, lambdas_range_end = 0.0001, 0.2
+    lambdas_range_start, lambdas_range_end = 0.0001, 0.5
     lambdas = np.linspace(lambdas_range_start, lambdas_range_end, n_evaluations)
     ridge_train_errors, lasso_train_errors, ridge_validation_errors, lasso_validation_errors = [], [], [], []
     for lam in lambdas:
@@ -99,7 +99,7 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
                                       mode="lines", name="Ridge Training Errors"),
                            go.Scatter(x=lambdas, y=ridge_validation_errors,
                                       mode="lines", name="Ridge Validation Errors")])
-    ridge_fig.update_layout(title=f"Errors of 5-Fold Cross-Validation on Ridge Regularisation with lambdas "
+    ridge_fig.update_layout(title=f"Errors of 5-Fold Cross-Validation on Ridge Regularization with lambdas "
                                   f"in range [{lambdas_range_start}, {lambdas_range_end}]<br>"
                                   f"Number of Samples in Train Set = {n_samples}",
                             xaxis_title="lambda", yaxis_title="Error Value")
@@ -107,7 +107,7 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
                                       mode="lines", name="Lasso Training Errors"),
                            go.Scatter(x=lambdas, y=lasso_validation_errors,
                                       mode="lines", name="Lasso Validation Errors")])
-    lasso_fig.update_layout(title=f"Errors of 5-Fold Cross-Validation on Lasso Regularisation with lambdas "
+    lasso_fig.update_layout(title=f"Errors of 5-Fold Cross-Validation on Lasso Regularization with lambdas "
                                   f"in range [{lambdas_range_start}, {lambdas_range_end}]<br>"
                                   f"Number of Samples in Train Set = {n_samples}",
                             xaxis_title="lambda", yaxis_title="Error Value")
